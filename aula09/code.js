df971e592009c480a516ba99242d756564d815a0
Rafaelsantos360 <github.com/rafaelsantos360>
2022-01-09 10:23:06 -0200

    aula09 de JavaScript

---
+++ aula09/code.js
@@ -0,0 +1,14 @@
+var body = window.document.body
+var p1 = window.document.getElementsByTagName('p')[1]
+body.style.background = 'black'
+/*
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+p1.style.color = 'blue'
+d.innerText = 'Estou agrardando...'
+*/
+var d = window.document.getElementsByName('msg')[0]
+d.innerText = 'Olá!'
+
+
+
