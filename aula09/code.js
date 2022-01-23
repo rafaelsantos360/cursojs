46fa2773d1a19d4e05e03447721d53a98fdb35dc
Rafaelsantos360 <github.com/rafaelsantos360>
2022-01-23 10:39:47 -0200

    iniviando a aula DOM

--- aula09/code.js
+++ aula09/code.js
@@ -1,14 +1,14 @@
 var body = window.document.body
-var p1 = window.document.getElementsByTagName('p')[1]
-body.style.background = 'black'
+var p1 = window.document.querySelectorAll('p')[1]
+body.style.background = 'red'
 /*
 var d = window.document.getElementById('msg')
 d.style.background = 'green'
 p1.style.color = 'blue'
 d.innerText = 'Estou agrardando...'
 */
-var d = window.document.getElementsByName('msg')[0]
-d.innerText = 'Olá!'
-
+var d = window.document.querySelector('div#msg')
+d.style.color = 'blue'
+d.style.background = 'green'
 
 
